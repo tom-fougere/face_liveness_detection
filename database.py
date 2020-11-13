37a85b88e590a7b1b159027013e880be9c3432b2
Tom <fougeretom@gmail.com>
2020-11-13 11:46:59 +0100

    Implement opencv face detection

---
+++ database.py
@@ -0,0 +1,41 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+while True:
+
+    # Capture the video frame
+    # by frame
+    _, frame = vid.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    profile = profile_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    for (x, y, w, h) in profile:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
